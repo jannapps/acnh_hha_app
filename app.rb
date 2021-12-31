7dbe9b711281b7a1cf802f689d06d4faa18f7480
Janna Harding <janna.harding01@gmail.com>
2021-12-31 07:33:39 -0500

    added basic checks to prevent exceptions when there are no search results

--- app.rb
+++ app.rb
@@ -12,7 +12,9 @@ get '/search' do
 
   begin
     connection = PG.connect :dbname => 'acnh_hha_app', :user => 'janna'
-    table = connection.exec 'SELECT * FROM housewares;'
+    table = nil 
+    
+    #connection.exec 'SELECT * FROM housewares;'
 
 #    table.each do |item|
 #      data.push({ name: item['name'], points: item['hha_base'] })
